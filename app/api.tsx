2f55ebcdf611eeeea6590ccf4a9b9ca0edf45c6d
yanu <10122431+ynwdd@users.noreply.github.com>
2022-05-10 10:45:51 +0700

    v0.57.2

--- app/api.tsx
+++ app/api.tsx
@@ -1,14 +1,16 @@
-import application, { response } from "https://deno.land/x/fastro@v0.57.0/server/mod.ts";
-import rendering from "https://deno.land/x/fastro@v0.57.0/server/ssr.ts";
+import application, {
+  response,
+} from "https://deno.land/x/fastro@v0.57.2/server/mod.ts";
+import rendering from "https://deno.land/x/fastro@v0.57.2/server/ssr.ts";
 import App from "./app.tsx";
 
 const ssr = rendering().component(<App />);
 
 const app = application(ssr)
-    .static("/static")
-    .get("/", () => {
-        return response(ssr).render({ title: "Hello world" });
-    });
+  .static("/static")
+  .get("/", () => {
+    return response(ssr).render({ title: "Hello world" });
+  });
 
 console.log("Listening on: http://localhost:8000");
 
