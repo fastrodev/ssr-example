d25255f92565a89b95740589855545e258e3c1a7
yanu <10122431+ynwdd@users.noreply.github.com>
2022-05-10 23:20:22 +0700

    update version

--- server.tsx
+++ server.tsx
@@ -1,15 +1,15 @@
+import rendering from "https://deno.land/x/fastro@v0.57.6/server/ssr.ts";
 import application, {
   response,
-} from "https://deno.land/x/fastro@v0.57.5/server/mod.ts";
-import rendering from "https://deno.land/x/fastro@v0.57.5/server/ssr.ts";
+} from "https://deno.land/x/fastro@v0.57.6/server/mod.ts";
 import App from "./components/app.tsx";
 
-const ssr = rendering().component(<App />);
+const component = rendering(<App />);
 
-const app = application(ssr)
+const app = application(component)
   .static("/static")
   .get("/", () => {
-    return response(ssr).render({ title: "Hello world" });
+    return response(component).render({ title: "Hello world" });
   });
 
 console.log("Listening on: http://localhost:8000");
